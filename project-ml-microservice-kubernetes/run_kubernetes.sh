8ca7c73586fe93fb66b30077b10ac03d8955b3cb
Shikha <shikhas@omnitracs.com>
2021-05-04 22:38:29 +0000

    modified docker and kubernetes files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=shikhas/demopred
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+minikube kubectl -- create deployment demopredict-app --image=docker.io/shikhas/demopred
 
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl -- get pods
+export POD_NAME=$(minikube kubectl -- get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+#echo name of the pod
+echo Name of the Pod: $POD_NAME
 
 # Step 4:
 # Forward the container port to a host
+minikube kubectl -- expose deployment/demopredict-app --type="NodePort" --port 8090
+minikube kubectl -- port-forward pod/$POD_NAME 8090:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=shikhas/demopred
 
 # Step 2:  
 # Authenticate & tag
+docker login --username shikhas
+docker tag demopred $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
